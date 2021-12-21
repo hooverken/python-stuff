647a94b6123b0525887407f72b3a97a51f7279ef
Ken Hoover <ken.hoover@microsoft.com>
2021-12-21 10:25:04 -0500

    simplified file handling

--- rotateImageFromBlobContainer.py
+++ rotateImageFromBlobContainer.py
@@ -18,31 +18,37 @@ default_credential = DefaultAzureCredential()
 
 blob_service_client = BlobServiceClient(account_url=f"https://{AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net", credential=default_credential)
 
+# Create a temporary file with the same extention (type) as the source file.  This will help Pillow
+# interpret the file as the correct image type when loading it.
+# pieces = AZURE_BLOB.split('.')
+# pieceCount = len(pieces)
+# inputFileNameExtension = pieces[pieceCount-1]
+# tempFile = tempfile.NamedTemporaryFile(suffix=inputFileNameExtension)
 
+# Download the blob to the temporary file
+# inputfile_blob_client = blob_service_client.get_blob_client(container=AZURE_INPUT_CONTAINER, blob=AZURE_BLOB)
+# try:
+#     tempFile.write(inputfile_blob_client.download_blob().readall())
+# except ResourceNotFoundError:
+#     print("Blob not found.")
 
-inputfile_blob_client = blob_service_client.get_blob_client(container=AZURE_INPUT_CONTAINER, blob=AZURE_BLOB)
-try:
-    tempFile.write(inputfile_blob_client.download_blob().readall())
-except ResourceNotFoundError:
-    print("Blob not found.")
+with open(AZURE_BLOB, "wb") as my_blob:
+    download_stream = inputfile_blob_client.download_blob()
+    my_blob.write(download_stream.readall())
 
-pieces = AZURE_BLOB.split('.')
-pieceCount = len(pieces)
-inputFileNameExtension = pieces[pieceCount-1]
-tempFile = tempfile.NamedTemporaryFile(suffix=inputFileNameExtension)
+# Open the file and rotate the image 90 degrees (counterclockwise), then save it back to disk
+NEW_BLOB_NAME = "ROTATED-" + AZURE_BLOB
+Image.open(AZURE_BLOB).rotate(90).save(NEW_BLOB_NAME)
 
-print("Input file has type " + inputFileNameExtension)
-Image.open(tempFile).rotate(90).save(tempFile)
-
-print("Rotated image saved to: " + tempFile.name)
+print("Rotated image saved to: " + NEW_BLOB_NAME)
 
 # Create a blob client using the local file name as the name for the blob
-outputfile_blob_client = blob_service_client.get_blob_client(container=AZURE_OUTPUT_CONTAINER, blob=tempFile.name)
+outputfile_blob_client = blob_service_client.get_blob_client(container=AZURE_OUTPUT_CONTAINER, blob=NEW_BLOB_NAME)
 
-print("\nUploading to Azure Storage as blob:\n\t" + tempFile.name)
+print("\nUploading to Azure Storage as blob:\n\t" + NEW_BLOB_NAME)
 
 # Upload the created file
-with open(tempFile, "rb") as data:
+with open(NEW_BLOB_NAME, "rb") as data:
     outputfile_blob_client.upload_blob(data)
 
-print("Upload of " + tempFile.name + " to Azure Storage complete.\n")
+print("Upload of " + NEW_BLOB_NAME + " to Azure Storage complete.\n")
